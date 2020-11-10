fe711168f01978daaeb424109d9e80ba0e467c81
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2020-11-09 21:23:42 -0500

    added server file with acid part 2

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+
 'use strict';
 const express = require('express');
 const app = express();
@@ -5,83 +6,91 @@ app.use(express.json()); // lets you handle JSON input
 const port = 3000;
 let datastore = {};
 
+const fs = require('fs');
+const join = require('path');
+const parse = require('url');
 let faker = require('faker');
 
-app.get('/', (req, res) => {
-  res.send('System Working!')
-});
 //   curl -d '{ "value" : "12" }' -H "Content-Type: application/json" http://localhost:3000/read/x
+
 app.get('/housing/new', (req, res) => {
-  const k = req.query.key;
-  const v = req.query.value;
-  datastore[k] = v;
-  console.log(`Set ${k} to ${v}`);
-  res.send('Set.');
+    const k = req.query.address;
+    const v = req.query.value;
+    datastore[k] = v;
+    console.log(`Set ${k} to ${v}`);
+    res.send('Review Written.');
 });
 app.get('/gym/new', (req, res) => {
-  const k = req.query.key;
-  const v = req.query.value;
-  datastore[k] = v;
-  console.log(`Set ${k} to ${v}`);
-  res.send('Set.');
+    const k = req.query.name;
+    const v = req.query.value;
+    datastore[k] = v;
+    console.log(`Set ${k} to ${v}`);
+    res.send('Review Written.');
 });
 app.get('/grocery/new', (req, res) => {
-  const k = req.query.key;
-  const v = req.query.value;
-  datastore[k] = v;
-  console.log(`Set ${k} to ${v}`);
-  res.send('Set.');
+    const k = req.query.name;
+    const v = req.query.value;
+    datastore[k] = v;
+    console.log(`Set ${k} to ${v}`);
+    res.send('Review Written.');
 });
 app.get('/laundromat/new', (req, res) => {
-  const k = req.query.key;
-  const v = req.query.value;
-  datastore[k] = v;
-  console.log(`Set ${k} to ${v}`);
-  res.send('Set.');
+    const k = req.query.name;
+    const v = req.query.value;
+    datastore[k] = v;
+    console.log(`Set ${k} to ${v}`);
+    res.send('Review Written.');
 });
 app.get('/review/new', (req, res) => {
-  const k = req.query.key;
-  const v = req.query.value;
-  datastore[k] = v;
-  console.log(`Set ${k} to ${v}`);
-  res.send('Set.');
+    const k = req.query.review;
+    const v = req.query.object;
+    datastore[k] = v;
+    console.log(`Set ${k} to ${v}`);
+    res.send('Review Written.');
 });
 app.get('/user/register', (req, res) => {
-  const k = req.query.key;
-  const v = req.query.value;
-  datastore[k] = v;
-  console.log(`Set ${k} to ${v}`);
-  res.send('Set.');
+    const k = req.query.username;
+    const v = req.query.password;
+    datastore[k] = v;  
+    console.log(`Set ${k} to ${v}`);
+    res.send('Account created.');
 });
-//housing write review
-app.get('/user/register', (req, res) => {
-  const k = req.query.key;
-  const v = req.query.value;
-  datastore[k] = v;
-  console.log(`Set ${k} to ${v}`);
-  res.send('Set.');
-});
-
-// app.get('/user/login', (req, res) => {
-//     const k = req.params['key'];
-//     const v = datastore[k];
-//     res.send(`key = ${k}, value = ${v}`);
-// });
 
 let randomName = faker.name.findName(); // Rowan Nikolaus
 let randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
 let randomCard = faker.helpers.createCard(); // random contact card containing many propert
+let randomAddress = faker.address.streetAddress();
 
-app.get('/search/:key', (req, res) => {
-  const k = req.params['key'];
-  const v = datastore[k];
-  res.send(`key = ${k}, value = ${randomName, randomEmail, randomCard}`);
-  // console.log(randomName, randomEmail, randomCard);
+app.get('/user/login', (req, res) => {
+    const k = req.params['username'];
+    const v = datastore[k];
+    console.log("User: " +k +" logged in.");
+    res.send(`username = ${randomName}, details = ${randomEmail}`);
 });
-app.get('/reviews/:key', (req, res) => {
-  const k = req.params['key'];
-  const v = datastore[k];
-  res.send(`key = ${k}, value = ${randomName, randomEmail, randomCard}`);
+
+app.get('/user/updateUsername', (req, res) => {
+    const k = req.params['old_username'];
+    datastore[k] = req.params['new_username'];
+    console.log("User: " +k +" updated.");
+    res.send(`username = ${randomName}, new_username = ${randomEmail}`);
+});
+
+app.get('/user/deleteAccount', (req, res) => {
+    const k = req.params['username'];
+    datastore[k] = null;
+    console.log("User: " +k +" deleted.");
+    res.send(`username = ${randomName} deleted.`);
+});
+
+app.get('/search/:item', (req, res) => {
+    const k = req.params['name'];
+    const v = datastore[k];
+    res.send(`item = ${k}, value = ${randomCard}`);
+});
+app.get('/reviews/:item', (req, res) => {
+    const k = req.params['name'];
+    const v = datastore[k];
+    res.send(`item = ${k}, reviews = ${randomCard}`);
 });
 app.get('/writeReview', (req, res) => {
   const k = req.query["name"];
@@ -90,7 +99,31 @@ app.get('/writeReview', (req, res) => {
   console.log(`Set ${k} to ${v}, body = ${JSON.stringify(req.query)}`);
   res.send('Set.');
 });
-/*app.get('*', (req, res) => {
-    res.send('NO FOOL');
-});*/
-app.listen(process.env.PORT || port);
+
+app.get('*',function(req,res) {
+    const parsed = parse.parse(req.url, true);
+    const filename = parsed.pathname === '/' ? "Ryan/home.html" : parsed.pathname.replace('/', '');
+        let path = join.join("", filename);
+        console.log("trying to serve " + path + "...");
+        if (fs.existsSync(path)) {
+            if (filename.endsWith("html")) {
+                res.writeHead(200, {"Content-Type" : "text/html"});
+            } else if (filename.endsWith("css")) {
+                res.writeHead(200, {"Content-Type" : "text/css"});
+            } else if (filename.endsWith("js")) {
+                res.writeHead(200, {"Content-Type" : "text/javascript"});
+            } else {
+                res.writeHead(200);
+            }
+
+            res.write(fs.readFileSync(path));
+            res.end();
+        } else {
+            res.writeHead(404);
+            res.end();
+        };
+});
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
