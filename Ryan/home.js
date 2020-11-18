366fbc53be44c71297197bb5ad66f4d951625bb4
jcanterbury1 <43765509+jcanterbury1@users.noreply.github.com>
2020-11-18 10:35:09 -0500

    Jason's Big Update

--- Ryan/home.js
+++
@@ -1,9 +0,0 @@
-
-
-async function search(){
-    const url = "/search" + document.getElementById("searchbar").value;
-    const response = await fetch(url);
-    if (!response.ok) {
-        console.error("Could not search.");
-    }
-}
--- Ryan/login.js
+++
@@ -1,8 +0,0 @@
-async function login() {
-  const username = document.getElementById("username").value;
-  const url = "/user/login" + username;
-  const response = await fetch(url);
-  if(!response.ok) {
-    console.error("Could not create account.");
-  }
-}
