aa45cea4172fc689a8ab5aab1813d156926df640
jcanterbury1 <>
2020-12-03 21:39:39 -0500

    Bug Fixes

--- server/server.js
+++ server/server.js
@@ -27,25 +27,8 @@ const strategy = new LocalStrategy(async (username, password, done) => {
   return done(null, username);
 });
 
-async function connectAndRun(task) {
-  let connection = null;
-
-  try {
-      connection = await db.connect();
-      return await task(connection);
-  } catch (e) {
-      throw e;
-  } finally {
-      try {
-          connection.done();
-      } catch(ignored) {
-
-      }
-  }
-}
-
 const app = express();
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 8000);
 app.use(expressSession(session));
 passport.use(strategy);
 app.use(passport.initialize());
@@ -63,58 +46,37 @@ app.use(express.urlencoded({ "extended": false }));
 
 
 
-
 //Ryan's endpoints and functions
-
-// Returns true iff the user exists.
-async function findUser(username) {
-  return (await connectAndRun(db => db.any("SELECT * FROM Users WHERE username=($1);", [username]))).length === 1;
+async function findUser(name) {
+  return ((await db.any("select * from users where username=($1);", [name])).length !== 0);
 }
-
-// Returns true iff the password is the one we have stored (in plaintext = bad but easy).
-async function validatePassword(name, pwd) {
-  if (!(await findUser(name))) {
-return false;
-  }
-  let fetch = await connectAndRun(db => db.any("SELECT hash, salt FROM Users WHERE username=($1);", [name]));
-  if (!mc.check(pwd, fetch[0].salt, fetch[0].hash)) {
-return false;
-  }
-  return true;
-}
-
-// Add a user to the "database".
 
 async function addUser(name, pwd) {
-  if (await findUser(name)) {
-return false;
+  if(await findUser(name)) {
+    return false;
   }
   const [salt, hash] = mc.hash(pwd);
-  console.log(salt);
-  await connectAndRun(db => db.none("INSERT INTO Users VALUES ($1, $2, $3);", [name, hash, salt]));
+  await db.none("insert into users values ($1,$2,$3);", [name, hash, salt]);
+  await db.none("insert into likes values ($1);", [name]);
   return true;
 }
 
-// Routes
+async function validatePassword(name, pwd) {
+  if(!(await findUser(name))) {
+    return false;
+  }
+  if(!mc.check(pwd, (await db.one("select salt from users where username=($1);", [name])).salt, (await db.one("select hash from users where username=($1);", [name])).hash)) {
+    return false;
+  }
+  return true;
+}
 
 function checkLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-// If we are authenticated, run the next route.
-next();
+  if(req.isAuthenticated()) {
+    next();
   } else {
-// Otherwise, redirect to the login page.
-res.redirect('/login');
+    res.redirect("/login");
   }
-}
-
-function checkNotLoggedIn(req, res, next) {
-if (!req.isAuthenticated()) {
-// If we are authenticated, run the next route.
-next();
-} else {
-// Otherwise, redirect to the login page.
-res.redirect('/home');
-}
 }
 
 app.post("/login", passport.authenticate("local", {
@@ -270,7 +232,7 @@ app.post("/likeListing", checkLoggedIn, async (req, res) => {
 
 
 // redirecting endpoints
-app.get("/login", checkNotLoggedIn, (req, res) => {
+app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "../client", "login.html"));
 });
 
@@ -279,7 +241,7 @@ app.get("/logout", checkLoggedIn, (req, res) => {
   res.redirect("/login");
 });
 
-app.get("/register", checkNotLoggedIn, (req, res) => {
+app.get("/register", (req, res) => {
   res.sendFile(path.join(__dirname, "../client", "create.html"));
 });
 
